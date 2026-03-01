ca78c80b497a670feb4f804c48649bdc3ad7ad81
authenticoder <authenticoderio@gmail.com>
2026-03-01 10:51:28 +0300

    brroooo

--- src/Summary/Summary.jsx
+++ src/Summary/Summary.jsx
@@ -3,10 +3,21 @@ import getResults from "../api/getResults";
 import "./summary.css";
 
 const Summary = () => {
-  const { data } = useQuery({
+  const { isLoading, data } = useQuery({
+    queryKey: ["summary"],
     queryFn: () => getResults(),
     staleTime: 30000,
   });
+
+  console.log(data);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
 
   const categoryColors = {
     Reaction: { bg: "var(--lighter-red)", text: "var(--light-red)" },
@@ -21,28 +32,27 @@ const Summary = () => {
   return (
     <div className="summary">
       <h1>Summary</h1>
-      {data &&
-        data.map(({ category, score, icon }) => {
-          const colors = categoryColors[category] || {};
-          return (
-            <div
-              className="category"
-              key={category}
-              style={{
-                backgroundColor: colors.bg,
-              }}
-            >
-              <div className="category-icon">
-                <img src={icon} alt={category} />
-                <p style={{ color: colors.text }}>{category}</p>
-              </div>
-              <div className="category-score">
-                <p>{score}</p>
-                <span>/100</span>
-              </div>
+      {data.map((summary) => {
+        const colors = categoryColors[summary.category] || {};
+        return (
+          <div
+            className="category"
+            key={summary.category}
+            style={{
+              backgroundColor: colors.bg,
+            }}
+          >
+            <div className="category-icon">
+              <img src={summary.icon} alt={summary.category} />
+              <p style={{ color: colors.text }}>{summary.category}</p>
             </div>
-          );
-        })}
+            <div className="category-score">
+              <p>{summary.score}</p>
+              <span>/100</span>
+            </div>
+          </div>
+        );
+      })}
       <button>Continue</button>
     </div>
   );
